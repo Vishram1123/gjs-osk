e94ca45f328db51bfa11b965cc172a04747ab4ff
vishram1123 <vish_2006@outlook.com>
2023-04-29 23:03:44 -0400

    formatted code

--- gjsosk@vishram1123.com/prefs.js
+++ gjsosk@vishram1123.com/prefs.js
@@ -1,29 +1,33 @@
 'use strict';
 
-const { Adw, Gio, Gtk, Gdk } = imports.gi;
+const {
+	Adw,
+	Gio,
+	Gtk,
+	Gdk
+} = imports.gi;
 
 const ExtensionUtils = imports.misc.extensionUtils;
 const Me = ExtensionUtils.getCurrentExtension();
 const UIFolderPath = Me.dir.get_child('ui').get_path();
 
-
 function init() {
-    let iconTheme = Gtk.IconTheme.get_for_display(Gdk.Display.get_default());
-  	iconTheme.add_search_path(UIFolderPath + `/icons`);
+	let iconTheme = Gtk.IconTheme.get_for_display(Gdk.Display.get_default());
+	iconTheme.add_search_path(UIFolderPath + `/icons`);
 }
 
 function fillPreferencesWindow(window) {
-	
-    const settings = ExtensionUtils.getSettings('org.gnome.shell.extensions.gjsosk');
-    
-    const page1 = new Adw.PreferencesPage({
-		title: "General", 
+
+	const settings = ExtensionUtils.getSettings('org.gnome.shell.extensions.gjsosk');
+
+	const page1 = new Adw.PreferencesPage({
+		title: "General",
 		icon_name: "general-symbolic"
 	});
-    
-    const group0 = new Adw.PreferencesGroup();
-    page1.add(group0)
-	
+
+	const group0 = new Adw.PreferencesGroup();
+	page1.add(group0)
+
 	const apply = Gtk.Button.new_with_label("Apply Changes");
 	apply.connect("clicked", () => {
 		settings.set_int("lang", langDrop.selected);
@@ -32,103 +36,126 @@ function fillPreferencesWindow(window) {
 		settings.set_int("portrait-height-percent", numChanger_pH.value);
 		settings.set_int("landscape-width-percent", numChanger_lW.value);
 		settings.set_int("landscape-height-percent", numChanger_lH.value);
-		let [r, g, b] = colorButton.get_rgba().to_string().replace("rgb(", "").replace(")","").split(",")
+		let [r, g, b] = colorButton.get_rgba().to_string().replace("rgb(", "").replace(")", "").split(",")
 		settings.set_double("background-r", r);
 		settings.set_double("background-g", g);
 		settings.set_double("background-b", b);
 		settings.set_int("font-size-px", numChanger_font.value);
 		settings.set_int("border-spacing-px", numChanger_bord.value);
-		settings.set_boolean("round-key-corners", dragToggle2.active); 
+		settings.set_boolean("round-key-corners", dragToggle2.active);
 		settings.set_int("default-snap", dropDown.selected);
 	});
 	group0.add(apply)
-	
-	const group1 = new Adw.PreferencesGroup({ title: "Behavior" });
-    page1.add(group1);
-	
-	const row0 = new Adw.ActionRow({ title: 'Language' });
-    group1.add(row0);
-    
-    let langList = ["QWERTY", "AZERTY", "Dvorak", "QWERTZ"];
-    let langDrop = Gtk.DropDown.new_from_strings(langList);
+
+	const group1 = new Adw.PreferencesGroup({
+		title: "Behavior"
+	});
+	page1.add(group1);
+
+	const row0 = new Adw.ActionRow({
+		title: 'Language'
+	});
+	group1.add(row0);
+
+	let langList = ["QWERTY", "AZERTY", "Dvorak", "QWERTZ"];
+	let langDrop = Gtk.DropDown.new_from_strings(langList);
 	langDrop.valign = Gtk.Align.CENTER;
 	langDrop.selected = settings.get_int("lang");
-	
+
 	row0.add_suffix(langDrop);
 	row0.activatable_widget = langDrop;
-	
-    const row1 = new Adw.ActionRow({ title: 'Enable Dragging' });
-    group1.add(row1);
-    
-	
-    const dragToggle = new Gtk.Switch({
-        active: settings.get_boolean('enable-drag'),
-        valign: Gtk.Align.CENTER,
-    });
 
-    row1.add_suffix(dragToggle);
-    row1.activatable_widget = dragToggle;
-    
-	const row2 = new Adw.ExpanderRow({ title: 'Portrait Sizing' });
-    group1.add(row2);
-    
-    let pW = new Adw.ActionRow({ title: 'Width (%)' })
-    let pH = new Adw.ActionRow({ title: 'Height (%)' })
-	
+	const row1 = new Adw.ActionRow({
+		title: 'Enable Dragging'
+	});
+	group1.add(row1);
+
+	const dragToggle = new Gtk.Switch({
+		active: settings.get_boolean('enable-drag'),
+		valign: Gtk.Align.CENTER,
+	});
+
+	row1.add_suffix(dragToggle);
+	row1.activatable_widget = dragToggle;
+
+	const row2 = new Adw.ExpanderRow({
+		title: 'Portrait Sizing'
+	});
+	group1.add(row2);
+
+	let pW = new Adw.ActionRow({
+		title: 'Width (%)'
+	})
+	let pH = new Adw.ActionRow({
+		title: 'Height (%)'
+	})
+
 	let numChanger_pW = Gtk.SpinButton.new_with_range(0, 100, 5);
 	numChanger_pW.value = settings.get_int('portrait-width-percent');
 	numChanger_pW.valign = Gtk.Align.CENTER;
-    pW.add_suffix(numChanger_pW);
+	pW.add_suffix(numChanger_pW);
 	pW.activatable_widget = numChanger_pW;
-	
+
 	let numChanger_pH = Gtk.SpinButton.new_with_range(0, 100, 5);
 	numChanger_pH.value = settings.get_int('portrait-height-percent');
 	numChanger_pH.valign = Gtk.Align.CENTER;
-    pH.add_suffix(numChanger_pH);
+	pH.add_suffix(numChanger_pH);
 	pH.activatable_widget = numChanger_pH;
-    
-    row2.add_row(pW);
-    row2.add_row(pH);
 
-    const row3 = new Adw.ExpanderRow({ title: 'Landscape Sizing' });
-    group1.add(row3);
-    
-    let lW = new Adw.ActionRow({ title: 'Width (%)' });
-    let lH = new Adw.ActionRow({ title: 'Height (%)' });
-	
+	row2.add_row(pW);
+	row2.add_row(pH);
+
+	const row3 = new Adw.ExpanderRow({
+		title: 'Landscape Sizing'
+	});
+	group1.add(row3);
+
+	let lW = new Adw.ActionRow({
+		title: 'Width (%)'
+	});
+	let lH = new Adw.ActionRow({
+		title: 'Height (%)'
+	});
+
 	let numChanger_lW = Gtk.SpinButton.new_with_range(0, 100, 5);
 	numChanger_lW.value = settings.get_int('landscape-width-percent');
 	numChanger_lW.valign = Gtk.Align.CENTER;
-    lW.add_suffix(numChanger_lW);
+	lW.add_suffix(numChanger_lW);
 	lW.activatable_widget = numChanger_lW;
-	
+
 	let numChanger_lH = Gtk.SpinButton.new_with_range(0, 100, 5);
 	numChanger_lH.value = settings.get_int('landscape-height-percent');
 	numChanger_lH.valign = Gtk.Align.CENTER;
-    lH.add_suffix(numChanger_lH);
+	lH.add_suffix(numChanger_lH);
 	lH.activatable_widget = numChanger_lH;
-    
-    row3.add_row(lW);
-    row3.add_row(lH);
-    
-    const row4 = new Adw.ActionRow({ title: 'Default Position' });
-    group1.add(row4);
-    
-    let posList = ["Top Left", "Top Center", "Top Right", "Center Left", "Center", "Center Right", "Bottom Left", "Bottom Center", "Bottom Right"];
-    let dropDown = Gtk.DropDown.new_from_strings(posList);
+
+	row3.add_row(lW);
+	row3.add_row(lH);
+
+	const row4 = new Adw.ActionRow({
+		title: 'Default Position'
+	});
+	group1.add(row4);
+
+	let posList = ["Top Left", "Top Center", "Top Right", "Center Left", "Center", "Center Right", "Bottom Left", "Bottom Center", "Bottom Right"];
+	let dropDown = Gtk.DropDown.new_from_strings(posList);
 	dropDown.valign = Gtk.Align.CENTER;
 	dropDown.selected = settings.get_int("default-snap");
-	
+
 	row4.add_suffix(dropDown);
 	row4.activatable_widget = dropDown;
-    
-    const group2 = new Adw.PreferencesGroup({ title: "Appearance" });
-    page1.add(group2);
-    
-    const row5 = new Adw.ActionRow({ title: 'Color' });
-    group2.add(row5);
-    
-    let rgba = new Gdk.RGBA();
+
+	const group2 = new Adw.PreferencesGroup({
+		title: "Appearance"
+	});
+	page1.add(group2);
+
+	const row5 = new Adw.ActionRow({
+		title: 'Color'
+	});
+	group2.add(row5);
+
+	let rgba = new Gdk.RGBA();
 	rgba.parse("rgba(" + settings.get_double("background-r") + ", " + settings.get_double("background-g") + ", " + settings.get_double("background-b") + ", 1)");
 	let colorButton = new Gtk.ColorButton({
 		rgba,
@@ -137,38 +164,44 @@ function fillPreferencesWindow(window) {
 	});
 	row5.add_suffix(colorButton);
 	row5.activatable_widget = colorButton;
-	
-	let row6 = new Adw.ActionRow({ title: 'Font Size (px)' });
+
+	let row6 = new Adw.ActionRow({
+		title: 'Font Size (px)'
+	});
 	group2.add(row6);
 
-	let numChanger_font= Gtk.SpinButton.new_with_range(0, 100, 1);
+	let numChanger_font = Gtk.SpinButton.new_with_range(0, 100, 1);
 	numChanger_font.value = settings.get_int('font-size-px');
 	numChanger_font.valign = Gtk.Align.CENTER;
-    row6.add_suffix(numChanger_font);
+	row6.add_suffix(numChanger_font);
 	row6.activatable_widget = numChanger_font;
-	
-	let row7 = new Adw.ActionRow({ title: 'Border Spacing (px)' });
+
+	let row7 = new Adw.ActionRow({
+		title: 'Border Spacing (px)'
+	});
 	group2.add(row7);
 
 	let numChanger_bord = Gtk.SpinButton.new_with_range(0, 10, 1);
 	numChanger_bord.value = settings.get_int('border-spacing-px');
 	numChanger_bord.valign = Gtk.Align.CENTER;
-    row7.add_suffix(numChanger_bord);
+	row7.add_suffix(numChanger_bord);
 	row7.activatable_widget = numChanger_bord;
-	
-	const row8 = new Adw.ActionRow({ title: 'Round Corners' });
-    group2.add(row8);
 
-    const dragToggle2 = new Gtk.Switch({
-        active: settings.get_boolean('round-key-corners'),
-        valign: Gtk.Align.CENTER,
-    });
+	const row8 = new Adw.ActionRow({
+		title: 'Round Corners'
+	});
+	group2.add(row8);
 
-    row8.add_suffix(dragToggle2);
-    row8.activatable_widget = dragToggle2;
-    
+	const dragToggle2 = new Gtk.Switch({
+		active: settings.get_boolean('round-key-corners'),
+		valign: Gtk.Align.CENTER,
+	});
+
+	row8.add_suffix(dragToggle2);
+	row8.activatable_widget = dragToggle2;
+
 	window.add(page1);
-	
+
 	let page2 = new Adw.PreferencesPage({
 		title: "About",
 		icon_name: 'info-symbolic',
@@ -198,7 +231,7 @@ function fillPreferencesWindow(window) {
 	});
 	let context = label.get_style_context();
 	context.add_class("title-1");
-	
+
 	let another_label = new Gtk.Label({
 		label: "Version " + Me.metadata.version
 	});
@@ -212,7 +245,6 @@ function fillPreferencesWindow(window) {
 		label: "Github",
 		uri: "https://github.com/Vishram1123/gjs-osk",
 	});
-	
 
 	code_row.add_suffix(github_link);
 	code_row.set_activatable_widget(github_link);
@@ -226,7 +258,7 @@ function fillPreferencesWindow(window) {
 
 	page2.add(contribute_icon_pref_group);
 	page2.add(links_pref_group);
-	
+
 	window.add(page2);
 	window.connect("close-request", () => {
 		settings.set_int("lang", langDrop.selected);
@@ -235,13 +267,13 @@ function fillPreferencesWindow(window) {
 		settings.set_int("portrait-height-percent", numChanger_pH.value);
 		settings.set_int("landscape-width-percent", numChanger_lW.value);
 		settings.set_int("landscape-height-percent", numChanger_lH.value);
-		let [r, g, b] = colorButton.get_rgba().to_string().replace("rgb(", "").replace(")","").split(",")
+		let [r, g, b] = colorButton.get_rgba().to_string().replace("rgb(", "").replace(")", "").split(",")
 		settings.set_double("background-r", r);
 		settings.set_double("background-g", g);
 		settings.set_double("background-b", b);
 		settings.set_int("font-size-px", numChanger_font.value);
 		settings.set_int("border-spacing-px", numChanger_bord.value);
-		settings.set_boolean("round-key-corners", dragToggle2.active); 
+		settings.set_boolean("round-key-corners", dragToggle2.active);
 		settings.set_int("default-snap", dropDown.selected);
 	});
 }
