8cbc1a66aa8baae5bf2d11abb25c1d93a7b45890
Vishal R <64966832+Vishram1123@users.noreply.github.com>
2023-10-02 00:41:49 +0000

    Update extension.js

--- gjsosk@vishram1123.com/extension.js
+++ gjsosk@vishram1123.com/extension.js
@@ -121,7 +121,7 @@ export default class GjsOskExtension extends Extension {
 		this._indicator = null;
 		this.openInterval = null;
 		if (this.settings.get_boolean("indicator-enabled")) {
-			this._indicator = new PanelMenu.Button(0.0, "${this.metadata.name} Indicator", false);
+			this._indicator = new PanelMenu.Button(0.0, "GJS OSK Indicator", false);
 			let icon = new St.Icon({
 				gicon: new Gio.ThemedIcon({
 					name: 'input-keyboard-symbolic'
