9d48d1c83dedbad4e767649066f3c5fbf2017c17
inunix3 <inunix3@tutamail.com>
2025-10-07 19:44:42 +0200

    include extended input layouts

--- genAllKeyMaps.sh
+++ genAllKeyMaps.sh
@@ -1,13 +1,13 @@
 dir="gjsosk@vishram1123.com/keycodes"
 rm -rf $dir
 mkdir $dir
-for i in $(localectl list-x11-keymap-layouts); do
+for i in $(python listKeyboardLayouts.py); do
 	echo $i
 	python genKeyMap.py "$i" > "$dir/$i.json"
 	for j in $(localectl list-x11-keymap-variants $i 2> /dev/null); do
 		echo "$i+$j"
 		python genKeyMap.py "$i+$j" > "$dir/$i+$j.json"
-	done; 
+	done;
 done;
 tar -Jcvf gjsosk@vishram1123.com/keycodes.tar.xz -C $dir/ .
 rm -rf $dir
