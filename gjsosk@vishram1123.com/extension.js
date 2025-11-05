1cad7252fb2e5bd9dbc221f7a241c08682204314
Vishram1123 <vish_2006@outlook.com>
2025-11-05 12:37:58 -0500

    Don't initialize keyboard if settings is null (fixes #123)

--- gjsosk@vishram1123.com/extension.js
+++ gjsosk@vishram1123.com/extension.js
@@ -336,6 +336,10 @@ export default class GjsOskExtension extends Extension {
                         this.Keyboard = null;
                     }
 
+                    if (this.settings == null) {
+                        return;
+                    }
+
                     function withErrorHandler(TargetClass, handler) {
                         return new Proxy(TargetClass, {
                             construct(Target, args) {
